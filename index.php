4c6f6143b109aafe5180ab0d5698ed2461399a02
dh711 <57220966+dh711@users.noreply.github.com>
2021-03-28 00:54:59 +0400

    Update index.php

--- index.php
+++ index.php
@@ -5,3 +5,4 @@ if(isset($_SESSION["loggedIn"]) && $_SESSION["loggedIn"] === true)
     header("Location: ./" . $_SESSION['role'] . "/index.php");
 else
     header("Location: ./login/login.php");
+?>
