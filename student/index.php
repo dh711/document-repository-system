8c51f93f0f96535065206431f9f75ef3b99ac945
dh711 <dhairya711@gmail.com>
2021-03-29 00:33:17 +0400

    added sessions.

--- student/index.php
+++ student/index.php
@@ -1,4 +1,11 @@
 <?php 
+session_start();
+ 
+if(isset($_SESSION["loggedIn"]) && $_SESSION["loggedIn"] === true)
+    echo $_SESSION["username"];
+else
+    header("Location: ../login/login.php");
+
 echo "Student Dashboard";
 echo "<a href='../login/logout.php'>logout?</a>";
 ?>
