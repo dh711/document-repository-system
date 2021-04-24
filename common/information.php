cb1722082f0d24fd20147d9dbaaec8d5d5d4731e
DJGARG7 <18BCE052@NIRMAUNI.AC.IN>
2021-04-24 18:21:10 +0530

    without css

--- common/information.php
+++ common/information.php
@@ -3,12 +3,46 @@ require_once("../classes/Database.php");
 
 Database::connect();
 
-print_r($_SESSION);
+
 $role = $_SESSION["role"];
 $query = "SELECT * FROM $role where email_id = ?;";
 $result = Database::query($query, 's', array($_SESSION['username']));
 $result = mysqli_fetch_array($result, MYSQLI_ASSOC);
-var_dump($result);
 
+echo('
+<center>
+    <table border="1">
+        <tr>
+            <th colspan="2">'.$role.' Information</th>
+        </tr>
+        <tr>
+            <td colspan="2">'.$result['email_id'].'</td>
+        </tr>
+        <tr>
+            <td>Roll No.</td>
+            <td>'.$result['rollno'].'</td>
+        </tr>
+        <tr>
+            <td>First Name</td>
+            <td>'.$result['first_name'].'</td>
+        </tr>
+        <tr>
+            <td>Middle Name</td>
+            <td>'.$result['last_name'].'</td>
+        </tr>
+        <tr>
+            <td>Address</td>
+            <td>'.$result['address'].'</td>
+        </tr>
+        <tr>
+            <td>Phone No.</td>
+            <td>'.$result['phone_no'].'</td>
+        </tr>
+        <tr>
+            <td>D.O.B.</td>
+            <td>'.$result['DOB'].'</td>
+        </tr>
+
+');
 Database::disconnect();
 ?>
