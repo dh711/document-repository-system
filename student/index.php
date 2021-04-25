66e070dbac8251e0c83be99dd075347ffbdb9376
dh711 <dhairya711@gmail.com>
2021-04-25 13:12:38 +0400

    changed fonts.

--- student/index.php
+++ student/index.php
@@ -30,6 +30,7 @@ else {
     <link rel="stylesheet" href="../styles/dashboard.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap">
     <script type="text/javascript" src="../scripts/tabs.js"></script>
     <title>Student Dashboard</title>
 </head>
